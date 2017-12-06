f75e10b5c0b86782294776e065bf876946fb1ff7
wolfpkmur <wolfpkmur@gmail.com>
2017-12-06 13:55:02 -0500

    danny
    guardado de datos a la base

--- proyectoIntegradorAccionesPersonal.AD/src/java/com/AccionesDePersonal/AD/cConsultasAD.java
+++ proyectoIntegradorAccionesPersonal.AD/src/java/com/AccionesDePersonal/AD/cConsultasAD.java
@@ -10,5 +10,13 @@ package com.AccionesDePersonal.AD;
  * @author alexa
  */
 public class cConsultasAD {
-    
+
+    public String strSQLInsert(String resolucion, String cedula, String apellido, String nombre) {
+        String result = "";
+        result = "INSERT INTO usuario(cedula,contrasena,nombre_trab,apellido_trab)"
+                + "VALUES(" + cedula + "," + "****" + "," + nombre + "," + apellido + ")";
+        return result;
+
+    }
+
 }
