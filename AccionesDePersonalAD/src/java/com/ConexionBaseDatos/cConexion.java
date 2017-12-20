7d6d65c7d2eb7c8cda3dbfb802ad3fac9c0d2347
wolfpkmur <wolfpkmur@gmail.com>
2017-12-20 17:27:47 -0500

    danny

--- AccionesDePersonalAD/src/java/com/ConexionBaseDatos/cConexion.java
+++ AccionesDePersonalAD/src/java/com/ConexionBaseDatos/cConexion.java
@@ -12,10 +12,20 @@ package com.ConexionBaseDatos;
 public class cConexion {
 
     public static String driverclass = "org.postgresql.Driver";
+
+//    Stteffano
 //    public static String databaseURL = "jdbc:postgresql://192.168.232.129:5432/db_acciones_personal";
-  //Vanessa Servidor 
-    public static String databaseURL = "jdbc:postgresql://192.168.81.128:5432/db_acciones_personal";
+//    public static String usuarioDB = "postgres";
+//    public static String claveDB = "12345";
+
+//    Vanessa Servidor 
+//    public static String databaseURL = "jdbc:postgresql://192.168.81.128:5432/db_acciones_personal";
+//    public static String usuarioDB = "postgres";
+//    public static String claveDB = "123456";
+
+//    Danny Servidor 
+    public static String databaseURL = "jdbc:postgresql://192.168.225.134:5432/db_acciones_personal";
     public static String usuarioDB = "postgres";
     public static String claveDB = "12345";
-
+    
 }
