25ebe8786b34000d6b95ba12c6f6c43bba2a9ca2
stteffano <stteffano_13@hotmail.com>
2017-12-20 18:01:55 -0500

    CONEXION

--- AccionesDePersonalAD/src/java/com/ConexionBaseDatos/cConexion.java
+++ AccionesDePersonalAD/src/java/com/ConexionBaseDatos/cConexion.java
@@ -12,39 +12,7 @@ package com.ConexionBaseDatos;
 public class cConexion {
 
     public static String driverclass = "org.postgresql.Driver";
-<<<<<<< HEAD
-
-    public static String databaseURL = "jdbc:postgresql://192.168.232.129:5432/db_acciones_personal";
-
-
-//    Stteffano
-//    public static String databaseURL = "jdbc:postgresql://192.168.232.129:5432/db_acciones_personal";
-//    public static String usuarioDB = "postgres";
-//    public static String claveDB = "12345";
-=======
     public static String databaseURL = "jdbc:postgresql://192.168.232.129:5432/db_acciones_personal";
     public static String usuarioDB = "postgres";
     public static String claveDB = "12345";
->>>>>>> f591a7e183f8e1e89cd9e4d8ce49181a7dbe2ecb
-
-//    Vanessa Servidor 
-//    public static String databaseURL = "jdbc:postgresql://192.168.81.128:5432/db_acciones_personal";
-//    public static String usuarioDB = "postgres";
-//    public static String claveDB = "123456";
-
-//    Danny Servidor 
-<<<<<<< HEAD
-    public static String databaseURL = "jdbc:postgresql://192.168.225.134:5432/db_acciones_personal";
-<<<<<<< HEAD
-
-=======
->>>>>>> f591a7e183f8e1e89cd9e4d8ce49181a7dbe2ecb
-    public static String usuarioDB = "postgres";
-    public static String claveDB = "123456";
-=======
-//    public static String databaseURL = "jdbc:postgresql://192.168.225.134:5432/db_acciones_personal";
-//    public static String usuarioDB = "postgres";
-//    public static String claveDB = "12345";
->>>>>>> 95742af31af5ddb22b706d346fb0e11ef51328a1
-    
 }
