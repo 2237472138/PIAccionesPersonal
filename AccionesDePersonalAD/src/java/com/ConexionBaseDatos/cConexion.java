31b4e4fd0a8e4f1f45510978a7a3ca1d1e78366e
vanessitaf8 <caryto_z@hotmail.com>
2017-12-20 17:15:16 -0500

    vanessa

--- AccionesDePersonalAD/src/java/com/ConexionBaseDatos/cConexion.java
+++ AccionesDePersonalAD/src/java/com/ConexionBaseDatos/cConexion.java
@@ -12,7 +12,9 @@ package com.ConexionBaseDatos;
 public class cConexion {
 
     public static String driverclass = "org.postgresql.Driver";
-    public static String databaseURL = "jdbc:postgresql://192.168.232.129:5432/db_acciones_personal";
+//    public static String databaseURL = "jdbc:postgresql://192.168.232.129:5432/db_acciones_personal";
+  //Vanessa Servidor 
+    public static String databaseURL = "jdbc:postgresql://192.168.81.128:5432/db_acciones_personal";
     public static String usuarioDB = "postgres";
     public static String claveDB = "12345";
 
